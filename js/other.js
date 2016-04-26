2b0b517af7b47d5f783e137fa9f5c24683857ff2
Dominik Bauer <mail@dominik-bauer.de>
2016-04-26 11:02:39 +0200

    more changes, parallax working and navbar added

--- js/other.js
+++ js/other.js
@@ -1,4 +1,17 @@
 
 $(document).ready(function(){
       $('.parallax').parallax();
+      $(".button-collapse").sideNav();
     });
+
+
+var options = [
+    {selector: '#about', offset: 50, callback: function() {
+
+      Materialize.toast("Lol awesome bro", 1500 );
+    } }
+    ];
+  Materialize.scrollFire(options);
+
+
+  
