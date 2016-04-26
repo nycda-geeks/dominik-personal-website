a23371d09bbe2d2e7546a7bb43737a08d587001a
Dominik Bauer <mail@dominik-bauer.de>
2016-04-26 18:25:16 +0200

    navbar appears when you scroll down, scrollspy

--- includes/other.js
+++ includes/other.js
@@ -1,7 +1,32 @@
 
 $(document).ready(function(){
-      $('.parallax').parallax();
-      });
+	$('.parallax').parallax();
+	// $('.slider').slider({full_width: true});
+	$('#navi').hide();
+	$('#about').scrollSpy();
+	$('#intro').scrollSpy();
+
+	$('#about').on('scrollSpy:enter', function() {
+		$('#navi').fadeIn(500);
+	});
+	
+	$('#intro').on('scrollSpy:enter', function() {
+		$('#navi').fadeOut(500);
+	});
+
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - 100
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
 
 /*
 var options = [
